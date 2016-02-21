f59a9c6270cd5da6e7444b9d920dd88816f48a61
Richard Fennell <richard@blackmarble.co.uk>
2016-02-21 23:31:56 +0000

    Modified the script to fail build if any errors seen

--- Tasks/Pester/Pester.ps1
+++ Tasks/Pester/Pester.ps1
@@ -9,4 +9,8 @@ $VerbosePreference ='Continue' # equiv to -verbose
 
 Import-Module $pwd\Pester.psd1
 Write-Verbose "Running Pester from $scriptFolder"
-Invoke-Pester -OutputFile $resultsFile -OutputFormat NUnitXml -Script $scriptFolder
+$result = Invoke-Pester -PassThru -OutputFile $resultsFile -OutputFormat NUnitXml -Script $scriptFolder
+if ($result.failedCount -ne 0)
+{ 
+    Write-Error "Pester returned errors"
+}
