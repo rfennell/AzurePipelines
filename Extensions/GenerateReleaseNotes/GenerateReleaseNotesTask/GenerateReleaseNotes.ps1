c1b0f5b5f79204eaaeccd00fd0771ba92a87ebde
Richard Fennell <richard@blackmarble.co.uk>
2016-05-18 17:35:57 +0100

    PowerShell altered to use a new stack based model to generate release notes that will work for build and release definitons

--- Extensions/GenerateReleaseNotes/GenerateReleaseNotesTask/GenerateReleaseNotes.ps1
+++ Extensions/GenerateReleaseNotes/GenerateReleaseNotesTask/GenerateReleaseNotes.ps1
@@ -55,7 +55,12 @@ function Get-BuildWorkItems
 
     $uri = "$($tfsUri)/$($teamproject)/_apis/build/builds/$($buildid)/workitems?api-version=2.0"
   	$jsondata = Invoke-GetCommand -uri $uri | ConvertFrom-Json
-  	$jsondata.value 
+    $wiList = @();
+  	foreach ($wi in $jsondata.value)
+    {
+       $wiList += Get-Detail -uri $wi.url
+    } 
+    $wiList
 }
 
 function Get-BuildChangeSets
@@ -69,8 +74,21 @@ function Get-BuildChangeSets
 
     $uri = "$($tfsUri)/$($teamproject)/_apis/build/builds/$($buildid)/changes?api-version=2.0"
   	$jsondata = Invoke-GetCommand -uri $uri | ConvertFrom-Json
-  	$jsondata.value 
-
+  	$csList = @();
+  	foreach ($cs in $jsondata.value)
+    {
+       # we can get more detail if the changeset is on VSTS or TFS
+       try {
+          $csList += Get-Detail -uri $cs.location
+       } catch
+       {
+          Write-warning "Unable to get details of changeset/commit as it is not stored in TFS/VSTS"
+          Write-warning "For [$($cs.id)] location [$($cs.location)]"
+          Write-warning "Just using the details we have from the build"
+          $csList += $cs
+       }
+    } 
+    $csList
 }
 
 function Get-Detail
@@ -109,6 +127,8 @@ function Get-Release
     $releaseid
     )
 
+    Write-Verbose "Getting details of release [$releaseid] from server [$tfsUri/$teamproject]"
+	
 	# fixup the URL
 	# bit of a hack whilst the RM API is in preview
 	$rmtfsUri = $tfsUri -replace ".visualstudio.com",  ".vsrm.visualstudio.com/defaultcollection"
@@ -118,7 +138,7 @@ function Get-Release
   	$jsondata
 }
 
-function Get-BuildsInRelease
+function Get-BuildIDsRelease
 {
 
     param
@@ -130,13 +150,22 @@ function Get-BuildsInRelease
 
 	# get the build IDs
     $buildIds = $release.artifacts.definitionReference.version.id
-	$builds = @()
-	foreach ($id in $buildIds)
-	{
-	  	write-verbose "Getting build details for BuildID [$id]"    
-	    $builds += Get-Build -tfsUri $tfsUri -teamproject $teamproject -buildid $id
-	}
-	$builds
+	$buildIds
+}
+
+function Indent-Space
+{
+    param(
+       $size =3,
+       $indent =1
+       )
+    
+    $upperBound = $size * $indent
+    for ($i =1 ; $i -le $upperBound  ; $i++)
+    {
+        $padding += " "
+    } 
+    $padding
 }
 
 function Invoke-GetCommand
@@ -155,7 +184,9 @@ function Invoke-GetCommand
     $webclient.DownloadString($uri)
 }
 
-function render() {
+
+
+function Render() {
     [CmdletBinding()]
     param ( [parameter(ValueFromPipeline = $true)] [string] $str)
 
@@ -189,90 +220,211 @@ function Get-Template
 	$template
 }
 
+function Get-Mode
+{
+    Param(
+       $line
+    )
+
+     $mode = [Mode]::BODY
+     if ($line.Trim() -eq "@@WILOOP@@") {$mode = [Mode]::WI}
+     if ($line.Trim() -eq "@@CSLOOP@@") {$mode = [Mode]::CS}
+	 if ($line.Trim() -eq "@@BUILDLOOP@@") {$mode = [Mode]::BUILD}
+     $mode
+}
+
+function Create-StackItem
+{
+
+    param
+    (
+        $items,
+        $modeStack,
+        $mode,
+        $index
+    )
+    # Create a queue of the items
+    $queue = new-object  System.Collections.Queue
+    # add each item to the queue  
+    foreach ($item in @($items))
+    {
+       $queue.Enqueue($item)
+    }
+    Write-Verbose "$(Indent-Space -indent ($modeStack.Count +1))$($queue.Count) items"
+    # place it on the stack with the blocks mode and start line index
+    $modeStack.Push(@{'Mode'= $mode;
+                      'BlockQueue'=$queue;
+                      'Index' = $index})
+}
+
 function Process-Template 
 {
 	Param(
 	  $template,
-	  $workItems,
-	  $changesets
+      $builds
 	)
 	
 	if ($template.count -gt 0)
 	{
-		write-Verbose "Processing template file"
-		$mode = [Mode]::BODY
-		#process each line
-		ForEach ($line in $template)
-		{
-			# work out if we need to loop on a blog
-			#Write-Verbose "Processing line [$line]"
-			if ($mode -eq [Mode]::BODY)
-			{
-				if ($line.Trim() -eq "@@WILOOP@@") {$mode = [Mode]::WI; continue}
-				if ($line.Trim() -eq "@@CSLOOP@@") {$mode = [Mode]::CS; continue}
-			} else {
-				if ($line.Trim() -eq "@@WILOOP@@") {$mode = [Mode]::BODY; continue}
-				if ($line.Trim() -eq "@@CSLOOP@@") {$mode = [Mode]::BODY; continue}
-			}
+		write-Verbose "Processing template"
 
-			switch ($mode)
-			{
-			"WI" {
-			if (@($workItems).count -gt 0) 
-				{
-					foreach ($wi in $workItems)
-					{
-					# Get the work item details so we can render the line
-					Write-Verbose "   Get details of workitem $($wi.id)"
-					$widetail = Get-Detail -uri $wi.url  
-					$out += $line | render
-					$out += "`n"
-					}
-				} else 
-				{
-					Write-Verbose "No associated work items found"
-					$out += "None`n"
-				}
-				continue
-				}
-			"CS" {
-				if (@($changesets).count -gt 0) 
-				{
-					foreach ($cs in $changesets)
-					{
-					# we can get enough detail from the list of changes
-					Write-Verbose "   Get details of changeset/commit $($cs.id)"
-					$csdetail = Get-Detail -uri $cs.location 
-					$out += $line | render
-					$out += "`n"
-					}
-				} else 
-				{
-					Write-Verbose "No associated changesets/commits found"
-					$out += "None`n"
-				}	
-				continue
-				} 
-			"BODY" {
-					# nothing to expand just process the line
-					$out += $line | render
-					$out += "`n"
-				}
+        # create our work stack and initialise
+		$modeStack = new-object  System.Collections.Stack 
+		$modeStack.Push([Mode]::BODY)
+
+        # this line is to provide support the the legacy build only template
+        # if using a release template it will be reset when processing tags
+        $builditem = $builds
+        
+		#process each line
+		For ($index =0; $index -lt $template.Count; $index++)
+		{
+            $line = $template[$index]
+            # get the line change mode if any
+            $mode = Get-Mode -line $line
+
+            #debug logging
+            #Write-Verbose "$Index[$($mode)]: $line"
+
+            if ($mode -ne [Mode]::BODY)
+            {
+                # is there a mode block change
+                if ($modeStack.Peek().Mode -eq $mode)
+                {
+                    # this means we have reached the end of a block
+                    # need to work out if there are more items to process 
+                    # or the end of the block
+                    $queue = $modeStack.Peek().BlockQueue;
+                    if ($queue.Count -gt 0)
+                    {
+                        # get the next item and initialise
+                        # the variables exposed to the template
+                        $item = $queue.Dequeue()
+                        # reset the index to process the block
+                        $index = $modeStack.Peek().Index
+                        switch ($mode)
+			            {
+			            "WI" {
+                            Write-Verbose "$(Indent-Space -indent $modeStack.Count)Getting next workitem $($item.id)"
+		                    $widetail = $item  
+                         }
+                         "CS" {
+                            Write-Verbose "$(Indent-Space -indent $modeStack.Count)Getting next changeset/commit $($item.id)"
+		                    $csdetail = $item 
+                         }
+                         "BUILD" {
+                            Write-Verbose "$(Indent-Space -indent $modeStack.Count)Getting next build $($item.build.id)"
+		                    $builditem = $item
+                            $build = $builditem.build # using two variables for legacy support
+                         }
+                         } #end switch
+                    }
+                    else
+                    {
+                        # end of block and no more items, so exit the block
+                        $mode = $modeStack.Pop().Mode
+                        Write-Verbose "$(Indent-Space -indent $modeStack.Count)Ending block $mode"
+                    }
+                } else {
+                    # this a new block to add the stack
+                    # need to get the items to process and place them in a queue
+                    Write-Verbose "$(Indent-Space -indent ($modeStack.Count))Starting block $($mode)"
+                ###    $queue = new-object  System.Collections.Queue  
+                    #set the index to jump back to
+                    $lastBlockStartIndex = $index       
+                    switch ($mode)
+			        {
+			            "WI" {
+                            # store the block and load the first item
+                            Create-StackItem -items @($builditem.workItems) -modeStack $modeStack -mode $mode -index $index
+                            if ($modeStack.Peek().BlockQueue.Count -gt 0)
+                            {
+                                $widetail = $modeStack.Peek().BlockQueue.Dequeue()
+                                Write-Verbose "$(Indent-Space -indent $modeStack.Count)Getting first workitem $($widetail.id)"
+                            } else {
+                                $widetail = $null
+                            }
+                         }
+                         "CS" {
+                            # store the block and load the first item
+                            Create-StackItem -items @($builditem.changesets) -modeStack $modeStack -mode $mode -index $index
+                            if ($modeStack.Peek().BlockQueue.Count -gt 0)
+                            {
+                               $csdetail = $modeStack.Peek().BlockQueue.Dequeue()   
+                               Write-Verbose "$(Indent-Space -indent $modeStack.Count)Getting first changeset/commit $($csdetail.id)"
+		                    
+                            } else {
+                                $csdetail = $null
+                            }
+                                                      
+                         }
+                        "BUILD" {
+                            Create-StackItem -items @($builds) -modeStack $modeStack -mode $mode -index $index
+                            if ($modeStack.Peek().BlockQueue.Count -gt 0)
+                            {
+                               $builditem = $modeStack.Peek().BlockQueue.Dequeue() 
+                               $build = $builditem.build
+                               Write-Verbose "$(Indent-Space -indent $modeStack.Count)Getting first build $($build.id)"
+		                   
+                            }  else {
+                                $builditem = $null
+                                $build = $null
+                            }
+                         }
+                    }
+                }
+            } else
+            {
+            if ((($modeStack.Peek().mode -eq [Mode]::WI) -and ($widetail -eq $null)) -or 
+                (($modeStack.Peek().mode -eq [Mode]::CS) -and ($csdetail -eq $null)))
+            {
+                # there is no data to expand
+                $out += "None"
+            } else {
+               	# nothing to expand just process the line
+				$out += $line | render
+				$out += "`n"
+                }
 			}
-		}
-		$out
+        }
+	    $out
 	} else
 	{
 		write-error "Cannot load template file [$templatefile] or it is empty"
 	} 
 }
 
+function Get-BuildDataSet
+{
+param
+(
+    $tfsUri,
+    $teamproject,
+    $buildid
+  )
+
+ 	write-verbose "Getting build details for BuildID [$buildid]"    
+ 	$build = Get-Build -tfsUri $collectionUrl -teamproject $teamproject -buildid $buildid
+
+    Write-Verbose "Getting associated work items for build [$($buildid)]"
+	Write-Verbose "Getting associated changesets/commits for build [$($buildid)]"
+
+    $build = @{'build'=$build;
+                'workitems'=(Get-BuildWorkItems -tfsUri $collectionUrl -teamproject $teamproject -buildid $buildid);
+                'changesets'=(Get-BuildChangeSets -tfsUri $collectionUrl -teamproject $teamproject -buildid $buildid )}
+    $build
+ }
+
+
+
+# types to make the switches neater
 Add-Type -TypeDefinition @"
    public enum Mode
    {
       BODY,
       WI,
-      CS
+      CS,
+      BUILD
    }
 "@
 
@@ -293,28 +445,27 @@ Write-Verbose "defname = [$env:BUILD_DEFINITIONNAME]"
 Write-Verbose "buildnumber = [$env:BUILD_BUILDNUMBER]"
 
 
-if ($releaseid -eq $null)
+if ( [string]::IsNullOrEmpty($releaseid))
 {
-	write-verbose "Getting build details for BuildID [$buildid]"    
-	$build = Get-Build -tfsUri $collectionUrl -teamproject $teamproject -buildid $buildid
-	$builds = @($build)
+    
+   Write-Verbose "In Build mode"
+   $builds = Get-BuildDataSet -tfsUri $collectionUrl -teamproject $teamproject -buildid $buildid
+    
 } else
 {
-	Write-Verbose "Getting details of release [$releaseid] from server [$collectionUrl/$teamproject]"
-	$release = Get-Release -tfsUri $collectionUrl -teamproject $teamproject -releaseid $releaseid
-	$builds = Get-BuildsInRelease -tfsUri $collectionUrl -teamproject $teamproject -release $release
-}
-
-foreach ($build in $builds)
-{
-	Write-Verbose "Getting associated work items for build [$($build.id)]"
-	$workitems = Get-BuildWorkItems -tfsUri $collectionUrl -teamproject $teamproject -buildid $($build.id) 
-	Write-Verbose "Getting associated changesets/commits for build [$($build.id)]"
-	$changesets = Get-BuildChangeSets -tfsUri $collectionUrl -teamproject $teamproject -buildid $($build.id) 
+	Write-Verbose "In Release mode"
+    $release = Get-Release -tfsUri $collectionUrl -teamproject $teamproject -releaseid $releaseid
+	# we put all the work items and changesets into an array associated with their build
+    $builds = @()
+  	foreach ($buildId in (Get-BuildIDsRelease -tfsUri $collectionUrl -teamproject $teamproject -release $release))
+	{
+		$builds += Get-BuildDataSet -tfsUri $collectionUrl -teamproject $teamproject -buildid $buildid
+	}
 }
 
 $template = Get-Template -templateLocation $templateLocation -templatefile $templatefile -inlinetemplate $inlinetemplate
-$outputmarkdown = Process-Template -template $template -workItems $workItems -changesets $changesets
+$outputmarkdown = Process-Template -template $template -builds $builds
+
 write-Verbose "Writing output file [$outputfile]."
 Set-Content $outputfile $outputmarkdown
 
