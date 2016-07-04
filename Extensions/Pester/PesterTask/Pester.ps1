0128505afd58ef0f23ec3d3610acf2ee5342d961
Richard Fennell <richard@blackmarble.co.uk>
2016-07-04 10:46:06 +0100

    Fixed the transform on the bool param

--- Extensions/Pester/PesterTask/Pester.ps1
+++ Extensions/Pester/PesterTask/Pester.ps1
@@ -1,25 +1,24 @@
+[CmdletBinding()]
 param
 (
     [string]$scriptFolder,
     [string]$resultsFile,
-    [boolean]$run32Bit 
+    [string]$run32Bit 
 )
 
-$VerbosePreference ='Continue' # equiv to -verbose
-
-if ($run32Bit -eq $false -and $env:Processor_Architecture -ne "x86")   
+if ($run32Bit -eq $true -and $env:Processor_Architecture -ne "x86")   
 {
     # Get the command parameters
     $args = $myinvocation.BoundParameters.GetEnumerator() | ForEach-Object {$($_.Value)}
-    write-warning 'Launching x86 PowerShell'
+    write-warning 'Re-launching in x86 PowerShell'
     &"$env:windir\syswow64\windowspowershell\v1.0\powershell.exe" -noprofile -executionpolicy bypass -file $myinvocation.Mycommand.path $args
     exit
 }
-write-verbose "Running in $($env:Processor_Architecture) PowerShell"
+write-verbose "Running in $($env:Processor_Architecture) PowerShell" -verbose
 
 
 Import-Module $pwd\Pester.psd1
-Write-Verbose "Running Pester from [$scriptFolder] output sent to [$resultsFile]"
+Write-Verbose "Running Pester from [$scriptFolder] output sent to [$resultsFile]" -verbose
 
 $result = Invoke-Pester -PassThru -OutputFile $resultsFile -OutputFormat NUnitXml -Script $scriptFolder
 if ($result.failedCount -ne 0)
