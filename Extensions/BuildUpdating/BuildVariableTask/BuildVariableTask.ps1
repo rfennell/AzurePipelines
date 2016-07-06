776ab9907c41736557ffdcc5cbf434fa5e8f6ce8
Richard Fennell <richard@blackmarble.co.uk>
2016-07-06 13:58:35 +0100

    Fixed image paths Chanhed authentication

--- Extensions/BuildUpdating/BuildVariableTask/BuildVariableTask.ps1
+++ Extensions/BuildUpdating/BuildVariableTask/BuildVariableTask.ps1
@@ -30,11 +30,10 @@ function Set-BuildDefinationVariable
 function Get-WebClient
 {
 
-    #$vssEndPoint = Get-ServiceEndPoint -Name "SystemVssConnection" -Context $distributedTaskContext
-    #$personalAccessToken = $vssEndpoint.Authorization.Parameters.AccessToken
+    $vssEndPoint = Get-ServiceEndPoint -Name "SystemVssConnection" -Context $distributedTaskContext
+    $personalAccessToken = $vssEndpoint.Authorization.Parameters.AccessToken
     $webclient = new-object System.Net.WebClient
-    #$webclient.Headers.Add("Authorization" ,"Bearer $personalAccessToken")
-    $webclient.UseDefaultCredentials = $true
+    $webclient.Headers.Add("Authorization" ,"Bearer $personalAccessToken")
     $webclient.Encoding = [System.Text.Encoding]::UTF8
     $webclient.Headers["Content-Type"] = "application/json"
 
