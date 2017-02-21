1554b6e10cb4d365bbe09a74340fe788b438caa9
Chris Gardner <chris@blackmarble.co.uk>
2017-02-21 15:00:28 +0000

    Adding Tag support
    Added support for the -Tag and -ExcludeTag parameters of Invoke-Pester. Moved over to using Splatting for the parameters to make it easier to build up the provided ones dynamically.
    
    Also added some tests for the script. Need more work on these in future iterations, particularly on the output testing.

--- Extensions/Pester/PesterTask/Pester.ps1
+++ Extensions/Pester/PesterTask/Pester.ps1
@@ -1,10 +1,43 @@
 [CmdletBinding()]
 param
 (
+    [Parameter(Mandatory)]
+    [ValidateScript({
+        Test-Path $_
+    })]
     [string]$scriptFolder,
+
+    [Parameter(Mandatory)]
+    [ValidateScript({
+        (Test-Path (Split-Path $_ -Parent)) -and ($_.split('.')[-1] -eq 'xml')
+    })]
     [string]$resultsFile,
+
     [string]$run32Bit,
-    [string]$moduleFolder
+
+    [validateScript({
+        if (Test-Path $_)
+        {
+            if (Get-ChildItem -Path $_ -Filter Pester.psd1)
+            {
+                $true
+            }
+            else
+            {
+                Throw "Pester.psd1 not found at path specified"
+            }
+        }
+        else 
+        {
+            Throw "Invalid path for ModuleFolder"
+        }
+        
+    })]
+    [string]$moduleFolder,
+
+    [string[]]$Tag,
+
+    [String[]]$ExcludeTag
 )
 
 
@@ -18,17 +51,42 @@ if ($run32Bit -eq $true -and $env:Processor_Architecture -ne "x86")
 }
 write-verbose "Running in $($env:Processor_Architecture) PowerShell" -verbose
 
-if ([string]::IsNullOrEmpty($moduleFolder))
+if ([string]::IsNullOrEmpty($moduleFolder) -and (-not(Get-Module -ListAvailable Pester)))
 {
     # we have no module path specified so use the copy we have in this task
     $moduleFolder = "$pwd\3.4.3"
+    Write-Verbose "Loading Pester module from [$moduleFolder]" -verbose
+    Import-Module $moduleFolder\Pester.psd1
+}
+elseif ($moduleFolder)
+{
+    Write-Verbose "Loading Pester module from [$moduleFolder]" -verbose
+    Import-Module $moduleFolder\Pester.psd1
+}
+else
+{
+    Import-Module Pester
 }
 
-Write-Verbose "Loading Pester module from [$moduleFolder]" -verbose
-Import-Module $moduleFolder\Pester.psd1
 Write-Verbose "Running Pester from [$scriptFolder] output sent to [$resultsFile]" -verbose
+$Parameters = @{
+    PassThru = $True
+    OutputFile = $resultsFile 
+    OutputFormat = 'NUnitXml' 
+    Script = $scriptFolder
+}
 
-$result = Invoke-Pester -PassThru -OutputFile $resultsFile -OutputFormat NUnitXml -Script $scriptFolder
+if ($Tag)
+{
+    $Parameters.Add('Tag',$Tag)
+}
+if ($ExcludeTag)
+{
+    $Parameters.Add('ExcludeTag',$ExcludeTag)
+}
+
+$result = Invoke-Pester @Parameters
+
 if ($result.failedCount -ne 0)
 { 
     Write-Error "Pester returned errors"
