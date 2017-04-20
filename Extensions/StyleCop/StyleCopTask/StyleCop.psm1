7f8c1473d6b06498a5441c7c3737a412dec03d2d
Richard Fennell <richard@blackmarble.co.uk>
2017-04-20 21:48:57 +0100

    Stylecop104 (#108)
    * Fix for finding settings file #104
    Addresses the problem when the settings.stylecop file is not in the root folder. Looks for the first instance of the file in a sub folder.
    If this is not the correct settings.stylecop file, due to multiple solutions in the directory structure, set the 'settingsFile' parameter"
    
    * Updated documentation
    
    * Put in more check logic for #104

--- Extensions/StyleCop/StyleCopTask/StyleCop.psm1
+++ Extensions/StyleCop/StyleCopTask/StyleCop.psm1
@@ -116,15 +116,23 @@ function Invoke-StyleCopForFolderStructure
                 $projectSettingsFile = $settings
             }  else
             {
-                $settings = Join-Path -path $sourcefolder -childpath "settings.stylecop"
-                if (Test-Path $settings)
+                # look for a stylecop.settings in a solution folder containing an SLN file
+                $pathToSln = (Get-ChildItem $sourcefolder -Filter *.sln -Recurse | Select-Object -First 1).Directory
+                if ($pathToSln -ne $null)
                 {
-                    Write-Verbose "Using settings.stylecop file in solution folder"
-                    $projectSettingsFile = $settings
-                } else 
-                {
-                    Write-Verbose "Cannot find a local settings.stylecop file, using default rules"
-                    $projectSettingsFile = "." # we have to pass something as this is a required param
+                    $settings = Join-Path -path $pathToSln -childpath "settings.stylecop"
+                    if (Test-Path $settings)
+                    {
+                        Write-Verbose "Using the first settings.stylecop file found in a solution folder"
+                        $projectSettingsFile = $settings
+                    } else 
+                    {
+                        Write-Verbose "Cannot find a settings.stylecop file in the same folder as the first SLN file, using default rules"
+                        $projectSettingsFile = "." # we have to pass something as this is a required param
+                    }
+                } else {
+                     Write-Verbose "Cannot find a folder containing an SLN file, using default rules"
+                     $projectSettingsFile = "." # we have to pass something as this is a required param
                 }
             }
         }
