18f0ea65d63231d093eb7a3e386356d40a85dbf0
Richard Fennell <richard@blackmarble.co.uk>
2017-04-30 20:35:22 +0100

    FIrst code complete version

--- Extensions/Versioning/VersionAssembliesTask.src/ApplyVersionToAssemblies.ts
+++ Extensions/Versioning/VersionAssembliesTask.src/ApplyVersionToAssemblies.ts
@@ -1,5 +1,6 @@
 import tl = require("vsts-task-lib/task");
 import fs = require("fs");
+import jschardet = require("jschardet");
 
 
 var path = tl.getInput("Path");
@@ -16,8 +17,29 @@ console.log (`Version Filter to extract build number: ${versionRegex}`);
 console.log (`Field to update (all if empty): ${field}`);
 console.log (`Output: Version Number Parameter Name: ${outputversion}`);
 
+// List all files in a directory in Node.js recursively in a synchronous fashion
+function findFiles (dir, filename , filelist) {
+  var path = path || require('path');
+  var fs = fs || require('fs'),
+      files = fs.readdirSync(dir);
+  filelist = filelist || [];
+  files.forEach(function(file) {
+    if (fs.statSync(path.join(dir, file)).isDirectory()) {
+      filelist = findFiles(path.join(dir, file), filename, filelist);
+    }
+    else {
+      console.log(`${file}   - ${filename} `);  
+      if (file.toLowerCase().endsWith(filename.toLowerCase()))
+      {
+        filelist.push(path.join(dir, file));
+      }
+    }
+  });
+  return filelist;
+};
+
 // Make sure path to source code directory is available
-if (fs.existsSync(path))
+if (!fs.existsSync(path))
 {
     tl.error(`Source directory does not exist: ${path}`);
     process.exit(1);
@@ -43,31 +65,42 @@ var newVersion = versionData[0]
 console.log (`Extracted Version: ${newVersion}`);
 
 // Apply the version to the assembly property files
-var files = Get-ChildItem path -recurse -include "*Properties*","My Project" | 
-    Where-Object { $_.PSIsContainer } | 
-    foreach { Get-ChildItem -Path $_.FullName -Recurse -include $FilenamePattern }
+var files = findFiles(`${path}`, filenamePattern, files); // ,filenamePattern needs to be added
 
-   console.log (`Will apply $NewVersion to ${files.count} files.`);
 
-    foreach (file in files) {
-        $FileEncoding = Get-FileEncoding -Path $File.FullName
-        $filecontent = Get-Content -Path $file.Fullname
-        attrib $file -r
-        if ([string]::IsNullOrEmpty($field))
+if (files.length>0) {
+
+    console.log (`Will apply ${newVersion} to ${files.length} files.`);
+
+    files.forEach(file => {
+        //        $FileEncoding = Get-FileEncoding -Path $File.FullName
+        var fileEncoding = jschardet.detect(fs.readFileSync(file));
+
+        var filecontent = fs.readFileSync(file, fileEncoding.encoding);
+        fs.chmodSync(file,"600");
+        if (field && field.length > 0)
         {
-            console.log ("Updating all version fields");
-            $filecontent -replace $VersionRegex, $NewVersion | Out-File $file -Encoding $FileEncoding
+            console.log (`Updating only the ${field} version`);
+            regexp = new RegExp(`${field}\\(\\"${versionRegex}`);
+            newVersion = `${field}(\"${newVersion}`;
         } else {
-            console.log ("Updating only the '$field' version");
-            $filecontent -replace "$field\(`"$VersionRegex", "$field(`"$NewVersion" | Out-File $file -Encoding $FileEncoding
+            console.log ("Updating all version fields");
+            regexp = new RegExp(versionRegex, "g"); // the g get all occurances
         }
-        
-        Write-Verbose "$file - version applied"
+        fs.writeFileSync(file,filecontent.toString().replace(regexp, newVersion),fileEncoding.encoding);
+        console.log (`${file} - version applied`);
+    });
+
+    if (outputversion && outputversion.length > 0)
+    {
+        console.log (`Set the output variable '${outputversion}' with the value ${newVersion}`);
+        tl.setVariable(outputversion, newVersion );
     }
-    Write-Verbose "Set the output variable '$outputversion' with the value $NewVersion"
-    Write-Host "##vso[task.setvariable variable=$outputversion;]$NewVersion"
 }
 else
 {
-    Write-Warning "Found no files."
+    tl.warning("Found no files.");
 }
+
+
+
