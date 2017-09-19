d7fe145d718687ea0040dad44249c8a4e8e076a5
Chris Gardner <chris@blackmarble.co.uk>
2017-09-19 21:15:58 +0100

    Fix tests to new format using parameters.

--- Tests/Extensions/Pester/PesterTask/Pester.Tests.ps1
+++ Tests/Extensions/Pester/PesterTask/Pester.Tests.ps1
@@ -5,28 +5,28 @@ Describe "Testing Pester Task" {
     Context "Testing Task Input" {
 
         it "Throws Exception when passed an invalid path for ScriptFolder" {
-            {&$sut -ScriptFolder TestDrive:\RandomFolder} | Should Throw
+            {&$sut -ScriptFolder TestDrive:\RandomFolder} | Should -Throw
         }
         it "ScriptFolder is Mandatory" {
-            (Get-Command $sut).Parameters['ScriptFolder'].Attributes.Mandatory | Should Be $True
+            (Get-Command $sut).Parameters['ScriptFolder'].Attributes.Mandatory | Should -Be $True
         }
         it "Throws Exception when passed an invalid location for ResultsFile" {
-            {&$sut -ScriptFolder TestDrive:\ -ResultsFile TestDrive:\RandomFolder } | Should Throw
+            {&$sut -ScriptFolder TestDrive:\ -ResultsFile TestDrive:\RandomFolder } | Should -Throw
         }
         it "Throws Exception when passed an invalid file type for ResultsFile" {
-            {&$sut -ScriptFolder TestDrive:\ -ResultsFile TestDrive:\output.xml} | Should Throw
+            {&$sut -ScriptFolder TestDrive:\ -ResultsFile TestDrive:\output.xml} | Should -Throw
         }
         it "ResultsFile is Mandatory" {
-            (Get-Command $sut).Parameters['ResultsFile'].Attributes.Mandatory | Should Be $True
+            (Get-Command $sut).Parameters['ResultsFile'].Attributes.Mandatory | Should -Be $True
         }
         it "Run32Bit is not Mandatory" {
-            (Get-Command $sut).Parameters['Run32Bit'].Attributes.Mandatory | Should Be $False
+            (Get-Command $sut).Parameters['Run32Bit'].Attributes.Mandatory | Should -Be $False
         }
         it "Throws Exception when passed an invalid path for ModuleFolder" {
-            {&$sut -ScriptFolder TestDrive:\ -ResultsFile TestDrive:\output.xml -ModuleFolder TestDrive:\RandomFolder} | Should Throw
+            {&$sut -ScriptFolder TestDrive:\ -ResultsFile TestDrive:\output.xml -ModuleFolder TestDrive:\RandomFolder} | Should -Throw
         }
         it "Throws Exception when passed a path which doesn't contain Pester for ModuleFolder" {
-            {&$sut -ScriptFolder TestDrive:\ -ResultsFile TestDrive:\output.xml -ModuleFolder TestDrive:\} | Should Throw
+            {&$sut -ScriptFolder TestDrive:\ -ResultsFile TestDrive:\output.xml -ModuleFolder TestDrive:\} | Should -Throw
         }
         it "Continues when passed a null ModuleFolder as VSTS task does" {
             mock Invoke-Pester { }
@@ -40,10 +40,10 @@ Describe "Testing Pester Task" {
 
         }
         it "ModuleFolder is not Mandatory" {
-            (Get-Command $sut).Parameters['ModuleFolder'].Attributes.Mandatory | Should Be $False
+            (Get-Command $sut).Parameters['ModuleFolder'].Attributes.Mandatory | Should -Be $False
         }
         it "Tag is not Mandatory" {
-            (Get-Command $sut).Parameters['Tag'].Attributes.Mandatory | Should Be $False
+            (Get-Command $sut).Parameters['Tag'].Attributes.Mandatory | Should -Be $False
         }
         it "Calls Invoke-Pester with multiple Tags specified" {
             mock Invoke-Pester { }
@@ -54,11 +54,11 @@ Describe "Testing Pester Task" {
 
             . $Sut -ScriptFolder TestDrive:\ -ResultsFile TestDrive:\output.xml -Tag 'Infrastructure,Integration'
             $Tag.Length | Should Be 2
-            Write-Output -NoEnumerate $Tag | Should BeOfType [System.Array]
-            Write-Output -NoEnumerate $Tag | Should BeOfType [String[]]
+            Write-Output -NoEnumerate $Tag | Should -BeOfType [System.Array]
+            Write-Output -NoEnumerate $Tag | Should -BeOfType [String[]]
         }
         it "ExcludeTag is not Mandatory" {
-            (Get-Command $sut).Parameters['ExcludeTag'].Attributes.Mandatory | Should Be $False
+            (Get-Command $sut).Parameters['ExcludeTag'].Attributes.Mandatory | Should -Be $False
         }
         it "Calls Invoke-Pester with multiple ExcludeTags specified" {
             mock Invoke-Pester { }
@@ -69,8 +69,8 @@ Describe "Testing Pester Task" {
 
             . $Sut -ScriptFolder TestDrive:\ -ResultsFile TestDrive:\output.xml -ExcludeTag 'Example,Demo'
             $ExcludeTag.Length | Should be 2
-            Write-Output -NoEnumerate $ExcludeTag | Should BeOfType [System.Array]
-            Write-Output -NoEnumerate $ExcludeTag | Should BeOfType [String[]]
+            Write-Output -NoEnumerate $ExcludeTag | Should -BeOfType [System.Array]
+            Write-Output -NoEnumerate $ExcludeTag | Should -BeOfType [String[]]
         }
 
         it "Handles CodeCoverageOutputFile being null from VSTS" {
@@ -138,7 +138,7 @@ Describe "Testing Pester Task" {
 
         it "Creates the output xml file correctly" {
             &$sut -ScriptFolder TestDrive:\ -ResultsFile TestDrive:\output.xml
-            Test-Path -Path TestDrive:\Output.xml | Should Be $True
+            Test-Path -Path TestDrive:\Output.xml | Should -Be $True
         }
         it "Throws an error when pester tests fail" {
             &$sut -ScriptFolder TestDrive:\ -ResultsFile TestDrive:\output2.xml
@@ -151,7 +151,7 @@ Describe "Testing Pester Task" {
             New-Item -Path TestDrive:\ -Name TestFile3.ps1 | Out-Null
 
             &$sut -ScriptFolder TestDrive:\ -ResultsFile TestDrive:\output.xml -CodeCoverageOutputFile 'TestDrive:\codecoverage.xml'
-            Test-Path -Path TestDrive:\codecoverage.xml | Should Be $True
+            Test-Path -Path TestDrive:\codecoverage.xml | Should -Be $True
         }
 
     }
