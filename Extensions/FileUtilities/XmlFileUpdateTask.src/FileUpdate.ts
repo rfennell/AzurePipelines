40be73d9d68e933a64b92f7dd313512a15f4871c
Richard Fennell <richard@blackmarble.co.uk>
2017-11-02 13:53:30 +0000

    Fixed TSLint issues

--- Extensions/FileUtilities/XmlFileUpdateTask.src/FileUpdate.ts
+++ Extensions/FileUtilities/XmlFileUpdateTask.src/FileUpdate.ts
@@ -1,21 +1,14 @@
 import tl = require("vsts-task-lib/task");
 import fs = require("fs");
 import path = require("path");
-var xpath = require('xpath') ,
-       dom = require('xmldom').DOMParser;
+const xpath = require("xpath") ,
+    dom = require("xmldom").DOMParser;
 
-
-var filename = tl.getInput("filename");
-var xpathQuery = tl.getInput("xpath");
-var attribute = tl.getInput("attribute");
-var value = tl.getInput("value");
-var recurse = new Boolean(tl.getInput("recurse"));
-
-filename = "c:\\tmp\\data*.xml";
-recurse = true;
-xpathQuery = "/catalog/book[1]/author";
-attribute = "";
-value = "<SUB>my name aaa</SUB>";
+const filename = tl.getInput("filename");
+const xpathQuery = tl.getInput("xpath");
+const attribute = tl.getInput("attribute");
+const value = tl.getInput("value");
+const recurse = new Boolean(tl.getInput("recurse"));
 
 console.log (`Param: filename - ${filename}`);
 console.log (`Param: recurse - ${recurse}`);
@@ -29,46 +22,43 @@ function escapeRegExp(str) {
 
 // List all files in a directory in Node.js recursively in a synchronous fashion
 function findFiles (dir, filenamePattern , recurse, filelist) {
-  
-  var regex = '^' + escapeRegExp(filenamePattern).replace(/\*/, '.*') + '$';
-  var r = new RegExp(regex, 'i'); // make it case insensitive
-  var path = path || require('path');
-  var fs = fs || require('fs'),
+  const regex = "^" + escapeRegExp(filenamePattern).replace(/\*/, ".*") + "$";
+  const r = new RegExp(regex, "i"); // make it case insensitive
+  // let path = path || require('path');
+  // let fs = fs || require('fs'),
 
-  folderItems = fs.readdirSync(dir);
+  const folderItems = fs.readdirSync(dir);
   filelist = filelist || [];
   folderItems.forEach(function(item) {
     if (fs.statSync(path.join(dir, item)).isDirectory()) {
-      // recurse down
-      if (recurse === true){
-        filelist = findFiles(path.join(dir, item), filenamePattern, recurse, filelist);
-      }
+       // recurse down
+       if (recurse === true) {
+          filelist = findFiles(path.join(dir, item), filenamePattern, recurse, filelist);
+       }
     }
     else {
-      if (r.test(item))
-      {
+      if (r.test(item)) {
         filelist.push(path.join(dir, item));
       }
     }
   });
   return filelist;
-};
+}
 
-var files = findFiles(path.dirname(filename), path.basename(filename), recurse, files); 
+let files;
+findFiles(path.dirname(filename), path.basename(filename), recurse, files);
 
 files.forEach(file => {
 
-    var rawContent = fs.readFileSync(file).toString();
-    var document = new dom().parseFromString(rawContent);   
+    let rawContent = fs.readFileSync(file).toString();
+    let document = new dom().parseFromString(rawContent);
 
-    var node = xpath.evaluate( xpathQuery ,document, null, xpath.XPathResult.FIRST_ORDERED_NODE_TYPE, null );
+    let node = xpath.evaluate( xpathQuery , document, null, xpath.XPathResult.FIRST_ORDERED_NODE_TYPE, null );
     if (node != null) {
-       if ((attribute == null) || (attribute.length ===0))
-        {
+       if ((attribute == null) || (attribute.length === 0)) {
             node.singleNodeValue.textContent = value;
             console.log(`Updated the file [${file}] with the new value [${xpathQuery}] with the value [${value}]`);
-        } else 
-        {
+        } else {
             node.singleNodeValue.setAttribute( attribute, value);
             console.log(`Updated the file [${file}] with the new value [${xpathQuery}] with the attribute [${attribute}=${value}]`);
         }
