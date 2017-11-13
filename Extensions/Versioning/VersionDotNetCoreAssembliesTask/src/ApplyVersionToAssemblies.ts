cb7bab5e12843213c775999a2d1dab4a55df3f29
Richard Fennell <richard@blackmarble.co.uk>
2017-11-13 16:28:12 +0000

    Moved the DOTNETCore versioner to new NPM build model #202

--- Extensions/Versioning/VersionDotNetCoreAssembliesTask/src/ApplyVersionToAssemblies.ts
+++ Extensions/Versioning/VersionDotNetCoreAssembliesTask/src/ApplyVersionToAssemblies.ts
@@ -2,7 +2,6 @@ import tl = require("vsts-task-lib/task");
 import fs = require("fs");
 import jschardet = require("jschardet");
 
-
 var path = tl.getInput("Path");
 var versionNumber = tl.getInput("VersionNumber");
 var versionRegex = tl.getInput("VersionRegex");
@@ -19,8 +18,8 @@ console.log (`Output: Version Number Parameter Name: ${outputversion}`);
 
 // List all files in a directory in Node.js recursively in a synchronous fashion
 function findFiles (dir, filename , filelist) {
-  var path = path || require('path');
-  var fs = fs || require('fs'),
+  var path = path || require("path");
+  var fs = fs || require("fs"),
       files = fs.readdirSync(dir);
   filelist = filelist || [];
   files.forEach(function(file) {
@@ -28,18 +27,16 @@ function findFiles (dir, filename , filelist) {
       filelist = findFiles(path.join(dir, file), filename, filelist);
     }
     else {
-      if (file.toLowerCase().endsWith(filename.toLowerCase()))
-      {
+      if (file.toLowerCase().endsWith(filename.toLowerCase())) {
         filelist.push(path.join(dir, file));
       }
     }
   });
   return filelist;
-};
+}
 
 // Make sure path to source code directory is available
-if (!fs.existsSync(path))
-{
+if (!fs.existsSync(path)) {
     tl.error(`Source directory does not exist: ${path}`);
     process.exit(1);
 }
@@ -47,14 +44,12 @@ if (!fs.existsSync(path))
 // Get and validate the version data
 var regexp = new RegExp(versionRegex);
 var versionData = regexp.exec(versionNumber);
-if (!versionData)
-{
+if (!versionData) {
     // extra check as we don't get zero size array but a null
     tl.error(`Could not find version number data in ${versionNumber} that matches ${versionRegex}.`);
     process.exit(1);
 }
-switch(versionData.length)
-{
+switch (versionData.length) {
    case 0:
          // this is trapped by the null check above
          tl.error(`Could not find version number data in ${versionNumber} that matches ${versionRegex}.`);
@@ -67,14 +62,13 @@ switch(versionData.length)
          break;
 }
 
-var newVersion = versionData[0]
+var newVersion = versionData[0];
 console.log (`Extracted Version: ${newVersion}`);
 
 // Apply the version to the assembly property files
 var files = findFiles(`${path}`, filenamePattern, files);
 
-
-if (files.length>0) {
+if (files.length > 0) {
 
     console.log (`Will apply ${newVersion} to ${files.length} files.`);
 
@@ -82,12 +76,11 @@ if (files.length>0) {
         var fileEncoding = jschardet.detect(fs.readFileSync(file));
 
         var filecontent = fs.readFileSync(file, fileEncoding.encoding);
-        fs.chmodSync(file,"600");
+        fs.chmodSync(file, "600");
 
          // Check that the field to update is present
         var tmpField = "<Version>";
-        if (field && field.length > 0)
-        {
+        if (field && field.length > 0) {
             tmpField = `<${field}>`;
         }
 
@@ -95,39 +88,34 @@ if (files.length>0) {
             console.log (`The ${tmpField} version is not present in the .csproj file so adding it`);
             // add the field, trying to avoid having to load library to parse xml
             // Check for TargetFramework when only using a single framework
-             regexp = new RegExp("</TargetFramework>","g")
-             tmpField = tmpField.replace('<','').replace('>','')
-             if (regexp.exec(filecontent.toString()))
-             {
+             regexp = new RegExp("</TargetFramework>", "g");
+             tmpField = tmpField.replace("<", "").replace(">", "");
+             if (regexp.exec(filecontent.toString())) {
                 console.log (`The ${file} .csproj file only targets 1 framework`);
                 var newVersionField = `</TargetFramework><${tmpField}>${newVersion}<\/${tmpField}>`;
-                fs.writeFileSync(file,filecontent.toString().replace(`</TargetFramework>`, newVersionField),fileEncoding.encoding);
+                fs.writeFileSync(file, filecontent.toString().replace(`</TargetFramework>`, newVersionField), fileEncoding.encoding);
              }
              // Check for TargetFrameworks when using multiple frameworks
-             regexp = new RegExp("</TargetFrameworks>","g")
-             if (regexp.exec(filecontent.toString()))
-             {
+             regexp = new RegExp("</TargetFrameworks>", "g");
+             if (regexp.exec(filecontent.toString())) {
                 console.log (`The ${file} .csproj file targets multiple frameworks`);
-                var newVersionField = `</TargetFrameworks><${tmpField}>${newVersion}<\/${tmpField}>`;
-                fs.writeFileSync(file,filecontent.toString().replace(`</TargetFrameworks>`, newVersionField),fileEncoding.encoding);
+                var newVersionField1 = `</TargetFrameworks><${tmpField}>${newVersion}<\/${tmpField}>`;
+                fs.writeFileSync(file, filecontent.toString().replace(`</TargetFrameworks>`, newVersionField1), fileEncoding.encoding);
              }
 
-        } else
-        {
-            if (field && field.length > 0)
-            {
+        } else {
+            if (field && field.length > 0) {
                 console.log (`Updating only the ${field} version`);
                 const csprojVersionRegex = `(<${field}>)(.*)(<\/${field}>)`;
-                var regexp = new RegExp(csprojVersionRegex,'gmi');
+                var regexp = new RegExp(csprojVersionRegex, "gmi");
                 let content: string = filecontent.toString();
                 let matches;
-                while ((matches = regexp.exec(content)) !== null)
-                {
-                    var existingTag: string = matches[0];
-                    console.log(`Existing Tag: ${existingTag}`);
-                    var replacementTag: string = `${matches[1]}${newVersion}${matches[3]}`;
-                    console.log(`Replacement Tag: ${replacementTag}`);
-                    content = content.replace(existingTag, replacementTag);
+                while ((matches = regexp.exec(content)) !== null) {
+                    var existingTag1: string = matches[0];
+                    console.log(`Existing Tag: ${existingTag1}`);
+                    var replacementTag1: string = `${matches[1]}${newVersion}${matches[3]}`;
+                    console.log(`Replacement Tag: ${replacementTag1}`);
+                    content = content.replace(existingTag1, replacementTag1);
                 }
                 fs.writeFileSync(file, content, fileEncoding.encoding);
 
@@ -136,8 +124,7 @@ if (files.length>0) {
                 const csprojVersionRegex = /(<\w+Version>)(.*)(<\/\w+Version>)/gmi;
                 let content: string = filecontent.toString();
                 let matches;
-                while ((matches = csprojVersionRegex.exec(content)) !== null)
-                {
+                while ((matches = csprojVersionRegex.exec(content)) !== null) {
                     var existingTag: string = matches[0];
                     console.log(`Existing Tag: ${existingTag}`);
                     var replacementTag: string = `${matches[1]}${newVersion}${matches[3]}`;
@@ -150,13 +137,10 @@ if (files.length>0) {
         }
     });
 
-    if (outputversion && outputversion.length > 0)
-    {
+    if (outputversion && outputversion.length > 0) {
         console.log (`Set the output variable '${outputversion}' with the value ${newVersion}`);
         tl.setVariable(outputversion, newVersion );
     }
-}
-else
-{
+} else {
     tl.warning("Found no files.");
 }
