543210de5fb2376f166376a54c3f19b291206451
Richard Fennell <richard@blackmarble.co.uk>
2017-11-18 15:19:29 +0000

    Added the option for a detailed summary #175

--- Extensions/StyleCop/task/StyleCop.ps1
+++ Extensions/StyleCop/task/StyleCop.ps1
@@ -10,11 +10,47 @@ param
     [string]$settingsFile,
     [string]$loggingfolder,
     [string]$summaryFileName,
-    [string]$sourcefolder 
+    [string]$sourcefolder,
+    [string]$detailedSummary 
 )
 
 $VerbosePreference ='Continue' # equiv to -verbose
 
+function Invoke-DetailedSummaryBuild {
+    Param(
+        [string]$loggingfolder,
+        [string]$sourceFolder
+    )
+
+    $text = ""
+    $timeout = 5
+    $extension = "*.xml"
+    $logsToScan = join-path $loggingfolder $extension
+    write-verbose "Waiting for logfiles to appear, can be a delay, will wait for up to $timeOut seconds"
+    $count =0
+    while((!(Test-Path $logsToScan)) -and ($count -le  $timeout)){
+        write-verbose ">" 
+        Start-Sleep -s 1;
+        $count ++
+    }
+    
+    # add an extra guard in case there are no logs
+    if (Test-Path $logsToScan){
+        Get-ChildItem  $loggingfolder -Filter $extension |
+        Foreach-Object {
+            $trimmedProjectName = $_.Name.Substring(0, $_.Name.IndexOf("."))
+            Write-Verbose "Writing details for project $trimmedProjectName"
+            $content = [xml](Get-Content($_.FullName))
+            $text += "### Project $trimmedProjectName `r`n"
+            foreach($item in $content.StyleCopViolations.Violation)
+            {
+                $trimFile = $($Item.Source).Replace("$sourcefolder\$trimmedProjectName\", "")
+                $text += "- $($item.RuleId) $($item.Rule) ($trimFile - Line No. $($item.LineNumber))`r`n" 
+            }
+        }
+    } 
+    return $text
+}
 
 import-module "Microsoft.TeamFoundation.DistributedTask.Task.Common" # to get the upload summary methods
 
@@ -32,16 +68,26 @@ import-module "Microsoft.TeamFoundation.DistributedTask.Task.Common" # to get th
             -loggingfolder $loggingfolder `
             -summaryFileName $summaryFileName `
             -sourcefolder $sourcefolder `
-            -verbose
-
+            -verbose 
+            
 # Set if the build should be failed or not getting the results from the file to avoid 32/64 bit issues
 $result = Import-Clixml $sourcefolder\results.xml
-			
+            
 # the output summary to the artifact folder and the VSTS summary
 $summaryMdPath = (join-path $loggingfolder  $summaryFileName)
 Write-Verbose ("Placing summary of test run in [{0}]" -f $summaryMdPath)
+
 Add-Content $summaryMdPath "###StyleCop"
-Add-Content $summaryMdPath ($result.Summary)
+
+# use the old summary
+if ([System.Convert]::ToBoolean($detailedSummary) -eq $false) {
+    Write-Verbose ("Using short summary")
+    Add-Content $summaryMdPath ($result.Summary)
+} else {
+    Write-Verbose ("Using detailed summary")
+    Add-Content $summaryMdPath (Invoke-DetailedSummaryBuild -loggingfolder $loggingfolder -sourcefolder  $sourcefolder)
+}
+
 Add-Content $summaryMdPath ("`nStyleCop found [{0}] violations across [{1}] projects" -f $result.TotalViolations, $result.ProjectsScanned)
 
 Write-verbose "Uploading summary results file"
