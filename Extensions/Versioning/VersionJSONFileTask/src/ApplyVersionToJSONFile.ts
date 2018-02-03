e60339eb31efeb6c3597bf7ca0546ce522b3a3b4
Richard Fennell <richard@blackmarble.co.uk>
2018-02-03 15:00:18 +0000

    Add option to to use a build number directly (#255)
    * Added option to use version directly
    * Updated readme

--- Extensions/Versioning/VersionJSONFileTask/src/ApplyVersionToJSONFile.ts
+++ Extensions/Versioning/VersionJSONFileTask/src/ApplyVersionToJSONFile.ts
@@ -13,10 +13,12 @@ var field = tl.getInput("Field");
 var outputversion = tl.getInput("outputversion");
 var filenamePattern = tl.getInput("FilenamePattern");
 var versionForJSONFileFormat = tl.getInput("versionForJSONFileFormat");
+var useBuildNumberDirectly = tl.getBoolInput("useBuildNumberDirectly");
 
 console.log (`Source Directory:  ${path}`);
 console.log (`Filename Pattern: ${filenamePattern}`);
 console.log (`Version Number/Build Number: ${versionNumber}`);
+console.log (`Use Build Number Directly: ${useBuildNumberDirectly}`);
 console.log (`Version Filter to extract build number: ${versionRegex}`);
 console.log (`Version Format for JSON File: ${versionForJSONFileFormat}`);
 console.log (`Field to update (all if empty): ${field}`);
@@ -28,31 +30,37 @@ if (!fs.existsSync(path)) {
     process.exit(1);
 }
 
-// Get and validate the version data
-var regexp = new RegExp(versionRegex);
-var versionData = regexp.exec(versionNumber);
-if (!versionData) {
-    // extra check as we don't get zero size array but a null
-    tl.error(`Could not find version number data in ${versionNumber} that matches ${versionRegex}.`);
-    process.exit(1);
-}
-switch (versionData.length) {
-   case 0:
-         // this is trapped by the null check above
-         tl.error(`Could not find version number data in ${versionNumber} that matches ${versionRegex}.`);
-         process.exit(1);
-   case 1:
-        break;
-   default:
-         tl.warning(`Found more than instance of version data in ${versionNumber}  that matches ${versionRegex}.`);
-         tl.warning(`Will assume first instance is version.`);
-         break;
-}
+// work out if we need to extract the verson from the build
+let jsonVersion = versionNumber; // set the default value
+if (useBuildNumberDirectly === false) {
+    // Get and validate the version data
+    var regexp = new RegExp(versionRegex);
+    var versionData = regexp.exec(versionNumber);
+    if (!versionData) {
+        // extra check as we don't get zero size array but a null
+        tl.error(`Could not find version number data in ${versionNumber} that matches ${versionRegex}.`);
+        process.exit(1);
+    }
+    switch (versionData.length) {
+    case 0:
+            // this is trapped by the null check above
+            tl.error(`Could not find version number data in ${versionNumber} that matches ${versionRegex}.`);
+            process.exit(1);
+    case 1:
+            break;
+    default:
+            tl.warning(`Found more than instance of version data in ${versionNumber}  that matches ${versionRegex}.`);
+            tl.warning(`Will assume first instance is version.`);
+            break;
+    }
 
-var buildVersion = versionData[0];
-console.log (`Extracted Build Version: ${buildVersion}`);
-
-const jsonVersion = getSplitVersionParts(versionRegex, versionForJSONFileFormat, buildVersion);
+    console.log (`Extracting version from the build number`);
+    var buildVersion = versionData[0];
+    console.log (`Extracted Build Version: ${buildVersion}`);
+    jsonVersion = getSplitVersionParts(versionRegex, versionForJSONFileFormat, buildVersion);
+} else {
+    console.log (`Using the provided build number without any further processing`);
+}
 console.log (`JSON Version Name will be: ${jsonVersion}`);
 
 // Apply the version to the assembly property files
--- Extensions/Versioning/readme.md
+++ Extensions/Versioning/readme.md
@@ -34,6 +34,7 @@
 - V1.29   - Issue #217 moved the ANdroid versioner to NodeJS
 - V1.30   - Issue #222 allowed more complex delimiters in version number format
 - V1.31   - Issue #233 added JSON and Angular versioner
+- V1.32   - Issue #254 allow version extraction to be bypassed for JSON Versioner 
 
 A set of tasks based on the versioning sample script to version tamping assemblies shown in the [VSTS documentation](https://msdn.microsoft.com/Library/vs/alm/Build/scripts/index
 ). These allow versioning of
