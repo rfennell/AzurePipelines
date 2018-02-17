c1fde2329cc6d2549c0452883f2dceb929b6d6cb
Richard Fennell <richard@blackmarble.co.uk>
2018-02-17 12:42:30 +0000

    Added more Pester task unit tests

--- Extensions/Pester/test/PesterTask/Helper.Tests.ps1
+++ Extensions/Pester/test/PesterTask/Helper.Tests.ps1
@@ -12,7 +12,13 @@ Describe "Testing Helper Functions" {
             $actual.count | Should -Be 0
         }
 
-        it "Can parse invalid block" {
+        it "Can parse block with no values but delimiter" {
+            $actual = Get-HashtableFromString -line ";"
+            $actual.GetType() | Should -Be @{}.GetType()
+            $actual.count | Should -Be 0
+        }
+
+        it "Cannot parse block with invalid string" {
             $actual = Get-HashtableFromString -line ";"
             $actual.GetType() | Should -Be @{}.GetType()
             $actual.count | Should -Be 0
@@ -20,6 +26,26 @@ Describe "Testing Helper Functions" {
 
         it "Can parse two part block" {
             $actual = Get-HashtableFromString -line "@{Path='C:\path\123'; Parameters=@{param1='111'; param2='222'}}"
+            $actual.Path | Should -Be "C:\path\123"
+            $actual.Parameters.param1 | Should -Be "111"
+            $actual.Parameters.param2 | Should -Be "222"
+        }
+
+        it "Can parse two hashtable part block" {
+            $actual = Get-HashtableFromString -line "@{Parameters1=@{param1='111'; param2='222'}; Parameters2=@{param1='111'; param2='222'}}"
+            $actual.Parameters1.param1 | Should -Be "111"
+            $actual.Parameters1.param2 | Should -Be "222"
+            $actual.Parameters2.param1 | Should -Be "111"
+            $actual.Parameters2.param2 | Should -Be "222"
+        }
+
+        it "Cannot parse block with non-matching brackets" {
+            # need the outer {} for =throw to work
+           { Get-HashtableFromString -line "@{Path='C:\path\123'; Parameters=@{param1='111'; param2='222'}" } | Should -Throw # ArgumentOutOfRangeException
+        }
+
+        it "Can parse block with trailing ;" {
+            $actual = Get-HashtableFromString -line "@{Path='C:\path\123'; Parameters=@{param1='111'; param2='222'}};"
             $actual.Path | Should -Be "C:\path\123"
             $actual.Parameters.param1 | Should -Be "111"
             $actual.Parameters.param2 | Should -Be "222"
