0e1ae16d9b4489beeb1a9c8c70785912c43ce643
Richard Fennell <richard@blackmarble.co.uk>
2018-03-13 18:58:17 +0000

    #281 altered the regex to allow SEMVER versioning

--- Extensions/Versioning/VersionJSONFileTask/test/VersionExtractionTest.ts
+++ Extensions/Versioning/VersionJSONFileTask/test/VersionExtractionTest.ts
@@ -23,4 +23,9 @@ describe ("Version number split tests", () => {
       expect(actual).to.equal("2017-12-5");
    });
 
+   it ("should be able to get a SEMVER style version", () => {
+    var actual = getSplitVersionParts ("\d+.\d+.\d+.\d+", "{1}.{2}.{3}-alfa.{4}", "2017.12.5.1");
+    expect(actual).to.equal("2017.12.5-alfa.1");
+ });
+
 });
