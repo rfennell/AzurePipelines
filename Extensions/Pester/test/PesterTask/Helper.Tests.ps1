0acdab892ea3a06c2037c0c2c200145f1a5874d8
Chris Gardner <Chris@blackmarble.co.uk>
2018-05-16 11:06:43 +0100

    Swap to using Language.Parser to more carefully parse the input string into a hashtable

--- Extensions/Pester/test/PesterTask/Helper.Tests.ps1
+++ Extensions/Pester/test/PesterTask/Helper.Tests.ps1
@@ -39,11 +39,6 @@ Describe "Testing Helper Functions" {
             $actual.Parameters2.param2 | Should -Be "222"
         }
 
-        it "Cannot parse block with non-matching brackets" {
-            # need the outer {} for =throw to work
-           { Get-HashtableFromString -line "@{Path='C:\path\123'; Parameters=@{param1='111'; param2='222'}" } | Should -Throw # ArgumentOutOfRangeException
-        }
-
         it "Can parse block with trailing ;" {
             $actual = Get-HashtableFromString -line "@{Path='C:\path\123'; Parameters=@{param1='111'; param2='222'}};"
             $actual.Path | Should -Be "C:\path\123"
@@ -52,7 +47,7 @@ Describe "Testing Helper Functions" {
         }
 
         it "Can parse three part block 1" {
-            $actual = Get-HashtableFromString -line "@{Path='C:\path\123';  x=y; Parameters=@{param1='111'; param2='222'}}"
+            $actual = Get-HashtableFromString -line "@{Path='C:\path\123';  x='y'; Parameters=@{param1='111'; param2='222'}}"
             $actual.Path | Should -Be "C:\path\123"
             $actual.Parameters.param1 | Should -Be "111"
             $actual.Parameters.param2 | Should -Be "222"
@@ -60,11 +55,30 @@ Describe "Testing Helper Functions" {
         }
 
         it "Can parse three part block 2" {
-            $actual = Get-HashtableFromString -line "@{Path='C:\path\123'; Parameters=@{param1='111'; param2='222'}; x=y}"
+            $actual = Get-HashtableFromString -line "@{Path='C:\path\123'; Parameters=@{param1='111'; param2='222'}; x='y'}"
             $actual.Path | Should -Be "C:\path\123"
             $actual.Parameters.param1 | Should -Be "111"
             $actual.Parameters.param2 | Should -Be "222"
             $actual.x | Should -Be "y"
+        }
+        it "Can parse hashtable with ; in a value" {
+            $actual = Get-HashtableFromString -line "@{Path='.\tests\script.tests.ps1'; Parameters=@{someVar='this'}},@{Path='.\tests\script2.tests.ps1'; Parameters=@{otherparam='foo.txt;bar.txt'}}"
+            $actual.GetType().BaseType | Should -Be "Array"
+            $actual[0].Path | Should -Be '.\tests\script.tests.ps1'
+            $actual[0].Parameters.SomeVar | Should -Be 'this'
+            $actual[1].Path | Should -Be '.\tests\script2.tests.ps1'
+            $actual[1].Parameters.otherparam | Should -Be 'foo.txt;bar.txt'
+        }
+        it "Can parse hashtable with commas in a value" {
+            $actual = Get-HashtableFromString -line "@{Path='.\tests\script.tests.ps1'; Parameters=@{someVar='this'}},@{Path='.\tests\script2.tests.ps1'; Parameters=@{otherparam='foo.txt;bar.txt';Param2='ValueGoesHere'}},@{path='.\tests\script3.tests.ps1';Parameters=@{inputvar='var,this,string'}}"
+            $actual.GetType().BaseType | Should -Be "Array"
+            $actual[0].Path | Should -Be '.\tests\script.tests.ps1'
+            $actual[0].Parameters.SomeVar | Should -Be 'this'
+            $actual[1].Path | Should -Be '.\tests\script2.tests.ps1'
+            $actual[1].Parameters.otherparam | Should -Be 'foo.txt;bar.txt'
+            $actual[1].Parameters.Param2 | Should -Be 'ValueGoesHere'
+            $actual[2].Path | Should -Be '.\tests\script3.tests.ps1'
+            $actual[2].Parameters.inputvar | Should -Be 'var,this,string'
         }
 
 
