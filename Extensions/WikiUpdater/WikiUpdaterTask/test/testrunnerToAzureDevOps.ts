fe9ab347ffc33587b1df48b1b2a5a8787cf976d8
Richard Fennell <richard@blackmarble.co.uk>
2018-11-20 09:41:06 +0000

    Wikiupdater (#396)
    Initial version of an extension that has a single task that takes a string input and writes it to a WIKI based on a GIT repo

---
+++ Extensions/WikiUpdater/WikiUpdaterTask/test/testrunnerToAzureDevOps.ts
@@ -0,0 +1,26 @@
+import {
+    UpdateGitWikiFile
+    } from "../src/GitWikiFuntions";
+
+function logInfo (msg: string) {
+    console.log(msg);
+}
+
+function logError (msg: string) {
+    console.log("\x1b[31m", msg);
+}
+
+// if using a PAT this is the instance name
+const user = "richardfennell";
+// If not using basic auth (bad) this must be a PAT
+const password = "<PAT>";
+
+const repo = "dev.azure.com/richardfennell/Git%20project/_git/Git-project.wiki";
+const localpath = "c:\\tmp\\test\\repo";
+const filename = "page.md";
+const contents = `Some text ${new Date().toString()}` ;
+const message = "A message";
+const gitname = "BuildProcess";
+const gitemail = "Build@Process";
+
+UpdateGitWikiFile(repo, localpath, user, password, gitname, gitemail, filename, message, contents, logInfo, logError);
