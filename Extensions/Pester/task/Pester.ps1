66712abf2d630d2c539c00e8ad437995b801339f
Chris Gardner <chris.gardner@section8games.co.uk>
2019-01-27 13:58:19 +0000

    Remove check for AllowPrelease as its not needed

--- Extensions/Pester/task/Pester.ps1
+++ Extensions/Pester/task/Pester.ps1
@@ -69,8 +69,7 @@ if ($PSBoundParameters.ContainsKey('additionalModulePath')) {
 }
 
 if ((Get-Module -Name PowerShellGet -ListAvailable) -and
-    (Get-Command Install-Module).Parameters.ContainsKey('SkipPublisherCheck') -and
-    (Get-Command Find-Module).Parameters.ContainsKey('AllowPrerelease')) {
+    (Get-Command Install-Module).Parameters.ContainsKey('SkipPublisherCheck')) {
 
     try {
         $null = Get-PackageProvider -Name NuGet -ErrorAction Stop
@@ -78,7 +77,7 @@ if ((Get-Module -Name PowerShellGet -ListAvailable) -and
     catch {
         Install-PackageProvider -Name Nuget -RequiredVersion 2.8.5.201 -Scope CurrentUser -Force -Confirm:$false
     }
-    $NewestPester = Find-Module -Name Pester -AllowPrerelease:$False | Sort-Object Version -Descending | Select-Object -First 1
+    $NewestPester = Find-Module -Name Pester | Sort-Object Version -Descending | Select-Object -First 1
     If ((Get-Module Pester -ListAvailable | Sort-Object Version -Descending| Select-Object -First 1).Version -lt $NewestPester.Version) {
         Install-Module -Name Pester -Scope CurrentUser -Force -Repository $NewestPester.Repository -SkipPublisherCheck
     }
