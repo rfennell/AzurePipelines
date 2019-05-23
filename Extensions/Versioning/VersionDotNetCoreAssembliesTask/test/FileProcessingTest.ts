dc8d7b1901d183b6ab96c316f63f227026521261
Richard Fennell <bm-richard.fennell@outlook.com>
2019-05-23 15:08:46 +0000

    Issue483 Version dotnetcore (#491)
    Restrict versions updated to main prportygroup to avoid orverwrite of nuget

--- Extensions/Versioning/VersionDotNetCoreAssembliesTask/test/FileProcessingTest.ts
+++ Extensions/Versioning/VersionDotNetCoreAssembliesTask/test/FileProcessingTest.ts
@@ -127,3 +127,26 @@ describe("Test the version extraction", () => {
   });
 
 });
+
+describe("Test the 483 file processing", () => {
+  before(function() {
+    // make a copy we can overwrite without breaking test data
+    copyFileSync("test/testdata/core483.csproj.initial", "test/testdata/core.csproj");
+  });
+
+  it("should be able to add a detail version field in a file", () => {
+    var file = "test/testdata/core.csproj";
+    ProcessFile(file, "Version", "9.9.9.9", true);
+
+    var editedfilecontent = fs.readFileSync(file);
+    var expectedfilecontent = fs.readFileSync(`test/testdata/core483.csproj.expected`);
+
+    expect(editedfilecontent.toString()).equals(expectedfilecontent.toString());
+  });
+
+  after(function() {
+    // remove the file if created
+    del.sync("test/testdata/*.csproj");
+  });
+
+});
