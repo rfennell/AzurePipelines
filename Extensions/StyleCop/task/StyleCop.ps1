dd1287de34e2e7abb7c290c4f88892796d4d2399
Richard Fennell <richard@blackmarble.com>
2019-07-28 09:36:01 +0100

    Swapped hander to PS3 style

--- Extensions/StyleCop/task/StyleCop.ps1
+++ Extensions/StyleCop/task/StyleCop.ps1
@@ -1,20 +1,23 @@
-param
-(
-    [string]$treatStyleCopViolationsErrorsAsWarnings,
-    [string]$maximumViolationCount,
-    [string]$allowableViolationCount,
-    [string]$showOutput,
-    [string]$cacheResults,
-    [string]$forceFullAnalysis,
-    [string]$additionalAddInPath,
-    [string]$settingsFile,
-    [string]$loggingfolder,
-    [string]$summaryFileName,
-    [string]$sourcefolder,
-    [string]$detailedSummary 
+[CmdletBinding()]
+param (
 )
 
 $VerbosePreference ='Continue' # equiv to -verbose
+
+Import-Module -Name "$PSScriptRoot\GenerateReleaseNotes.psm1" -Force
+
+$treatStyleCopViolationsErrorsAsWarnings = Get-VstsInput -Name "treatStyleCopViolationsErrorsAsWarnings"
+$maximumViolationCount = Get-VstsInput -Name "maximumViolationCount"
+$allowableViolationCount = Get-VstsInput -Name "allowableViolationCount"
+$showOutput = Get-VstsInput -Name "showOutput"
+$cacheResults = Get-VstsInput -Name "cacheResults"
+$forceFullAnalysis = Get-VstsInput -Name "forceFullAnalysis"
+$additionalAddInPath = Get-VstsInput -Name "additionalAddInPath"
+$settingsFile = Get-VstsInput -Name "settingsFile"
+$loggingfolder = Get-VstsInput -Name "loggingfolder"
+$summaryFileName = Get-VstsInput -Name "summaryFileName"
+$sourcefolder = Get-VstsInput -Name "sourcefolder"
+$detailedSummary = Get-VstsInput -Name "detailedSummary"
 
 function Invoke-DetailedSummaryBuild {
     Param(
@@ -51,8 +54,6 @@ function Invoke-DetailedSummaryBuild {
     } 
     return $text
 }
-
-import-module "Microsoft.TeamFoundation.DistributedTask.Task.Common" # to get the upload summary methods
 
 # Run the script that does the work in a separate script
 # so we can force it to be loaded in 32bit PowerShell as this is required for disctionary loading
