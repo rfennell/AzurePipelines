bed05a56c06905dca3f66255518c883bf54e336c
Andy Ward <andy.ward@xbim.net>
2019-11-05 14:31:53 +0000

    Use wildcard regex as fall back when finding Json files (#570)
    * Use wildcard regex as fall back when matching files.
    * Fixes #569
    * Updated Task help with the new regex feature

--- Extensions/Versioning/VersionJSONFileTask/test/FileDiscoveryTest.ts
+++ Extensions/Versioning/VersionJSONFileTask/test/FileDiscoveryTest.ts
@@ -13,13 +13,23 @@ const del = require("del");
 
 describe ("Find files tests", () => {
 
-  it ("should be able to find 4 files with recursion", () => {
+  it ("should be able to find 6 files with recursion", () => {
       var filelist = findFiles ("test/testdata", "package.json.initial" , filelist, true);
-      expect(filelist.length).to.equal(4);
+      expect(filelist.length).to.equal(6);
   });
 
-  it ("should be able to find 1 file with no recursion", () => {
+  it ("should be able to find 2 files with no recursion", () => {
     var filelist = findFiles ("test/testdata", "package.json.initial" , filelist, false);
+    expect(filelist.length).to.equal(2);
+  });
+
+  it ("should be able to find 4 files with recursion and wildcard", () => {
+    var filelist = findFiles ("test/testdata", "^package.json.initial" , filelist, true);
+    expect(filelist.length).to.equal(4);
+  });
+
+  it ("should be able to find 1 file with no recursion and wildcard", () => {
+    var filelist = findFiles ("test/testdata", "^package.json.initial" , filelist, false);
     expect(filelist.length).to.equal(1);
-});
+  });
 });
