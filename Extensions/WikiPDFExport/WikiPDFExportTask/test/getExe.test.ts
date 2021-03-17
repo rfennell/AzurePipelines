ee8e2c9bf300b7ced25e64cf44fd16d04a62c006
Richard Fennell <richard@blackmarble.co.uk>
2021-03-17 20:40:25 +0000

    Fixed tests

--- Extensions/WikiPDFExport/WikiPDFExportTask/test/getExe.test.ts
+++ Extensions/WikiPDFExport/WikiPDFExportTask/test/getExe.test.ts
@@ -7,22 +7,22 @@ const del = require("del");
 describe("Get Exe Path ", () => {
   it("should be able download the release on windows", async () => {
     var actual = await GetExePath("", ".\\testdata", true);
-    expect(actual).to.equal(`".\\testdata\\azuredevops-export-wiki.exe"`);
+    expect(actual).to.equal(`.\\testdata\\azuredevops-export-wiki.exe`);
   }).timeout(20000);     // the default is 2000ms and that is too fast or the download
 
   it("should be able download the release on non-windows", async () => {
     var actual = await GetExePath("", ".\\testdata", false);
-    expect(actual).to.equal(`"./testdata/azuredevops-export-wiki.exe"`);
+    expect(actual).to.equal(`./testdata/azuredevops-export-wiki.exe`);
   }).timeout(20000);     // the default is 2000ms and that is too fast or the download
 
   it("should be able to override the release download", async () => {
     var actual = await GetExePath(".\\testdata\\dummy.exe.txt", "", true);
-    expect(actual).to.equal(`".\\testdata\\dummy.exe.txt"`);
+    expect(actual).to.equal(`.\\testdata\\dummy.exe.txt`);
   });
 
   it("should be able to override the release download", async () => {
     var actual = await GetExePath(".\\testdata\\dummy.exe.txt", "", true);
-    expect(actual).to.equal(`".\\testdata\\dummy.exe.txt"`);
+    expect(actual).to.equal(`.\\testdata\\dummy.exe.txt`);
   });
 
   after(() => {
