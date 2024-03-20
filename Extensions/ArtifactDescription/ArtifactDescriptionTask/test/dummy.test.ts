8b91208a06f27b73b24f476affe1e929fe450e31
Richard Fennell <bm-richard.fennell@outlook.com>
2024-03-20 16:37:13 +0000

    Update tsconfig watch setting

--- Extensions/ArtifactDescription/ArtifactDescriptionTask/test/dummy.test.ts
+++ Extensions/ArtifactDescription/ArtifactDescriptionTask/test/dummy.test.ts
@@ -1,1 +1,7 @@
-// Empty file to stop standard build failing
+import "jest";
+
+describe("A empty set of tests to check the CI/CD system", () => {
+    it("should be able to compare two numbers", () => {
+      expect(1 === 1);
+    });
+  });
