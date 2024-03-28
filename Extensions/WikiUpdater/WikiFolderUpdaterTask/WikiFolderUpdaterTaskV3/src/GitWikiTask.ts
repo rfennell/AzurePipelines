737916d360455ed21829fa095dd863270bee4b38
Richard Fennell <bm-richard.fennell@outlook.com>
2024-03-28 15:10:35 +0000

    Package both the V2 and V3 extension (#1836)
    * Package both the V2 and V3 extension in a single VSIX
    * This provides ongoing support for the AZDO Agent V2.x Node10 agents and the Agent V3. Node16 agent
    * Fixes issue in WIKIFolderUpdate  with error in Node Modules
    * The massive version leap of the V3 task in this package is due to an issue in testing and that release numbers must always increment to be publishable to the AZDO Marketplace

---
+++ Extensions/WikiUpdater/WikiFolderUpdaterTask/WikiFolderUpdaterTaskV3/src/GitWikiTask.ts
@@ -0,0 +1,96 @@
+import tl = require("azure-pipelines-task-lib/task");
+import * as fs from "fs";
+
+import {
+    UpdateGitWikiFolder,
+    GetTrimmedUrl,
+    GetProtocol
+} from "./GitWikiFuntions";
+
+import {
+    logInfo,
+    logError,
+    getSystemAccessToken
+} from "./agentSpecific";
+
+var repo = tl.getInput("repo");
+var targetFolder = tl.getInput("targetFolder");
+var localpath = tl.getInput("localpath");
+var message = tl.getInput("message");
+var gitname = tl.getInput("gitname");
+var gitemail = tl.getInput("gitemail");
+var user = tl.getInput("user");
+var password = tl.getInput("password");
+var useAgentToken = tl.getBoolInput("useAgentToken");
+var replaceFile = tl.getBoolInput("replaceFile");
+var appendToFile = tl.getBoolInput("appendToFile");
+var sourceFolder = tl.getInput("sourceFolder");
+var filter = tl.getInput("filter");
+var tagRepo = tl.getBoolInput("tagRepo");
+var tag = tl.getInput("tag");
+var branch = tl.getInput("branch");
+var injectExtraHeader = tl.getBoolInput("injectExtraHeader");
+var sslBackend = tl.getInput("sslBackend");
+var retriesInput = tl.getInput("retries");
+var mode = tl.getInput("RetryMode");
+
+var retries = 5;
+try {
+    retries = Number(retriesInput);
+} catch {
+    console.log(`Count not parse the inputed retry count of ${retriesInput} set it to the default of ${retries} `);
+}
+
+console.log(`Variable: Repo [${repo}]`);
+console.log(`Variable: TargetFolder [${targetFolder}]`);
+console.log(`Variable: Commit Message [${message}]`);
+console.log(`Variable: Git Username [${gitname}]`);
+console.log(`Variable: Git Email [${gitemail}]`);
+console.log(`Variable: Use Agent Token [${useAgentToken}]`);
+console.log(`Variable: Replace File [${replaceFile}]`);
+console.log(`Variable: Append to File [${appendToFile}]`);
+console.log(`Variable: Username [${user}]`);
+console.log(`Variable: Password [*****]`);
+console.log(`Variable: Localpath [${localpath}]`);
+console.log(`Variable: SourceFolder [${sourceFolder}]`);
+console.log(`Variable: Filter [${filter}]`);
+console.log(`Variable: Tag Repo [${tagRepo}]`);
+console.log(`Variable: Tag [${tag}]`);
+console.log(`Variable: Branch [${branch}]`);
+console.log(`Variable: InjectExtraHeader [${injectExtraHeader}]`);
+console.log(`Variable: SslBackend [${sslBackend}]`);
+console.log(`Variable: Retries [${retries}]`);
+console.log(`Variable: Mode [${mode}]`);
+
+if (useAgentToken === true) {
+    console.log(`Using OAUTH Agent Token, overriding username and password`);
+    user = "buildagent";
+    password = getSystemAccessToken();
+}
+
+var protocol = GetProtocol(repo, logInfo);
+repo = GetTrimmedUrl(repo, logInfo);
+
+UpdateGitWikiFolder(
+    protocol,
+    repo,
+    localpath,
+    user,
+    password,
+    gitname,
+    gitemail,
+    targetFolder,
+    message,
+    sourceFolder,
+    filter,
+    logInfo,
+    logError,
+    replaceFile,
+    appendToFile,
+    tagRepo,
+    tag,
+    injectExtraHeader,
+    sslBackend,
+    branch,
+    retries,
+    mode);
